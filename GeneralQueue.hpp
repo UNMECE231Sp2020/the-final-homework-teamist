2e66901c78be9b5f4d21e3679322a8ceac56a322
root <root@DESKTOP-D9GDID1.localdomain>
2020-04-25 15:00:04 -0600

    Attempted at this. Added the functions and multiple for loops, if statements and completed the templates for the bool operators with the friend functions. It is a little similar to the Stack file as well.

--- GeneralQueue.hpp
+++ GeneralQueue.hpp
@@ -10,33 +10,166 @@ class Queue {
 		size_t _size;
 	public:
 		//Default constructor
-		Queue();
+		Queue() {
+			_size = 0;
+			_data = nullptr;
+		}
 
 		//Copy constructor
-		Queue(const Queue &q);
+		Queue(const Queue &q) {
+			if(q.size() == 0) {
+				_size = 0;
+				_data = nullptr;
+			}
+			else{
+				int q_size = q.size();
+				List *q_data_copy = new List[q_size];
+				int i;
+				for(i=0; i<q_size; i++) {
+					*(q_data_copy+i) = *(q._data+i);
+				}
+
+				if(q_size == 1){
+					_data = new List(*q_data_copy);
+				}
+				else{
+					_data = new List[q_size];
+					for(i=0; i<q_size; i++) {
+						*(_data+i) = *(q_data_copy+i);
+					}
+				}
+				_size = q_size;
+				delete [] q_data_copy;
+			}
+		}
 
 		//Getters
-		size_t size() const;
+		size_t size() const {
+			return _size;
+		}
 
-		Q front() const;
+		Q front() const {
+			return *(_data+size()-1);
+		}
 
-		Q back() const;
+		Q back() const {
+			return *_data;
+		}
 
 		//Push to queue
-		void enqueue(Q value);
+		void enqueue(Q value) {
+			if(_data == nullptr) {
+				_data = new List(data);
+				_size++;
+			}
+			else{
+				int data_size = size();
+				List *data_copy = new List[data_size]; 
+				int i;
+				for(i=0; i<data_size; i++) {
+					*(data-copy+i) = *(_data+i);
+				}
+
+				delete [] _data;
+				_data = new List [data_size+1]; 
+
+				*(_data) = data;
+				for(i=0; i<data_size; i++) {
+					*(_data+i+1) = *(data_copy+i); 
+				}
+				_size++;
+				delete[] data_copy;
+			}
+		}
 
 		//Pop from queue
-		void dequeue();
+		void dequeue() {
+			if(_data == nullptr) {
+				std::cout << "Could not pop empty Queue, Master Yoda" << std::endl;
+				return;
+			}
+			else if(size() == 1) {
+				delete _data;
+				_data = nullptr;
+				_size = 0;
+			}
+			else{
+				int data_size = size();
+				List *data_copy = new List[data_size-1];
+				int i;
+				for(i=0; i<data_size-1; i++) {
+					*(data_copy+i) = *(_data+i);
+				}
 
-		void print();
+				delete [] _data;
+				_data = new List[data_size-1];
 
-		bool search(Q value);
+				for(i=0; i<data_size-1; i++) {
+					*(_data+i) = *(data-copy+i);
+				}
+				delete [] data_copy;
+				_size--;
+			}
+		}
 
-		bool empty();
+
+		void print() {
+			for(int i=0; i<size(); i++) {
+				std::cout << *(_data+i) << " ";
+			}
+			std::cout << std::endl;
+		}
+
+		bool search(Q value) {
+			for(int i=0; i<size(); i++) {
+				if(*(_data+i) == value) {
+					return true;
+				}
+			}
+			return false;
+		}
+
+
+		bool empty() {
+			return _data == nullptr;
+		}
 
 		Queue<Q> operator=(const Queue<Q> q) {
-			_data = q._data;
-			_size = _data.size();
+			if(q.size() == 0) {
+				return *this;
+			}
+			if(q.size() == 1) {
+				List data_copy = *q.values;
+				if(size() < 2) {
+					delete _data;
+				}
+				else{
+					delete [] _data;
+				}
+				_data = new List(data_copy);
+				_size = 1;
+			}
+			else {
+				int data_size = q.size();
+				List *data_copy = new List[data_size];
+				for(int i=0; i<data_size; i++) {
+					*(data_copy+i) = *(q._data+i);
+				}
+				
+				if(size() < 2) {
+					delete _data;
+				}
+				else{
+					delete [] _data;
+				}
+				_data = new List [data_size];
+
+				for(int i=0; i<data_size; i++) {
+					*(_data+i) = *(data_copy+i);
+				}
+				_size = data_size;
+				delete [] data_copy;
+			}
 		}
 
 		template <class U>
@@ -51,3 +184,29 @@ class Queue {
 		friend bool operator!=(const Queue<U> &left_queue, 
 				const Queue<U> &right_queue);
 };
+
+template <class U>
+std::ostream &operator <<(std::ostream &out, const Queue<U> &q) {
+	for(int i=0l i<q.size(); i++) {
+		out << *(q._data+i) << " ";
+	}
+	return out;
+}
+
+template <class U>
+bool operator==(const Queue<U> &left_queue, const Queue<U> &right_queue) {
+	if(left_queue.size() != right_queue.size()) {
+		return false;
+	}
+	for(int i=0; i<left_queue.size(); i++) {
+		if(*(left_queue._data+i) != *(right_queue._data+i)) {
+			return false;
+		}
+	}
+	return true;
+}
+
+template <class U>
+bool operator!=(const Queue<U> &left_queue, const Queue<U> &right_queue) {
+	return !=(left_queue==right_queue);
+}
